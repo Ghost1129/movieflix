7de9df36ed2ea2799ea51e8f3a18b07b333f7eef
Ghost <1129.gh0st@gmail.com>
2023-01-03 09:02:58 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MainPage from './pages/MainPage';
+import MovieDetail from './pages/MovieDetail';
+import { createContext, useState } from 'react';
+import { AuthContextProvider } from './context/AuthContext';
+import Protected from './pages/Protected';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContextProvider>
+    <Routes>
+      <Route path="/" element={
+        <Protected>
+          <Login/>
+        </Protected>
+      } />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/home" element={<MainPage/>} />
+        <Route path="/:movieId" element={<MovieDetail/>} />
+    </Routes>
+    </AuthContextProvider>
+    
   );
 }
 
